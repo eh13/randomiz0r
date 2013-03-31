572ca07193d09fa3bafa0c5bfd68502a7e4cbe09
Baer <kathlen.korn@gmail.com>
2013-03-31 01:21:18 +0100

    bbb

--- src/der_programm/main.c
+++ src/der_programm/main.c
@@ -4,16 +4,19 @@
 #include <util/delay.h>
 #include <inttypes.h>
 
+static uint8_t digits[10] = {0b11000000,0b11111001,0b10100100,0b10110000,0b10011001,
+                             0b10010010,0b10000010,0b11111000,0b10000000,0b10010000};
+
 int main(void)
 {
 
     DDRA=0xff;
-
-    while(1) {
-        PORTA = 0xff;
-        _delay_ms(500);
-        PORTA = 0;
-        _delay_ms(500);
+    uint8_t i;
+    while(1){
+	for(i=0;i<10;i++){
+		PORTA = digits[i];
+		_delay_ms(1000);
+	}
     }
 
     return 0;
